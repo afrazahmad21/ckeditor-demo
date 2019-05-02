37acdf262ccc2342848429ba09fd850aa3a23e23
Afraz Ahmad <afraz.ahmed@origamistudios.us>
2019-05-02 13:29:10 +0500

    build created

---
+++ build/precache-manifest.973d04b8d08499b1198180d382d690d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e0941e68c348e5a0e8ab271ab60fce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c76d95b4fc2ce45970",
+    "url": "/static/css/main.043a1152.chunk.css"
+  },
+  {
+    "revision": "dec272343b4e2abb46ec",
+    "url": "/static/js/2.57bf3869.chunk.js"
+  },
+  {
+    "revision": "f1c76d95b4fc2ce45970",
+    "url": "/static/js/main.d3c796d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
