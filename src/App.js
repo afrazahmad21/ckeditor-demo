195391560119aeb1532039ae7bccc9bc989eca02
Afraz Ahmad <afraz.ahmed@origamistudios.us>
2019-05-02 12:08:46 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import axios from 'axios';
+import {SaveCKHtml} from './apiUrls';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        dataCK: "<p>Edit Here</p>"
+    }
+    onChange = (event, editor) => {
+        const data = editor.getData();
+        console.log({event, editor, data});
+        this.setState({dataCK: data})
+    }
+
+    onSubmit = (e) => {
+        const {dataCK} = this.state;
+        axios.post(SaveCKHtml, {data: dataCK}, {
+            auth: {
+                username: 'ckeditor',
+                password: 'pakistan123'
+            }
+        })
+            .then(response => {
+                const {message} = response.data
+                alert(message)
+            }).catch(err => {
+                debugger
+            const {message} = err.data
+            alert(message)
+        })
+    }
+
+    render() {
+        let {dataCK} = this.state;
+        dataCK = dataCK.split('\\n').join('\n');
+
+        return (
+            <div className="App">
+                <h2> CKEditor 5 </h2>
+                <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Edit here</p>"
+                    onInit={editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log('Editor is ready to use!', editor);
+                    }}
+                    onChange={this.onChange}
+                />
+
+                <div style={{marginTop: "20px", fontWeight: "bold"}}>
+                    <label>Code</label>
+                </div>
+                <div style={{marginTop: "10px"}}>
+                    <textarea rows={7} cols={100} value={dataCK}/>
+                </div>
+                <div>
+                    <button type={"button"} onClick={this.onSubmit}>Save HTML</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
+
---
+++ src/apiUrls.js
@@ -0,0 +1,2 @@
+const baseUrl = 'http://localhost:3010';
+export const SaveCKHtml = `${baseUrl}/save`;
